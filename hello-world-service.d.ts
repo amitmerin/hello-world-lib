1bf0c05b054099378d10ff61f24bbfd394810e2e
Amit <amit.merin@emc.com>
2018-03-27 19:08:31 -0400

    add declerations

---
+++ hello-world-service.d.ts
@@ -0,0 +1,11 @@
+/**
+ *
+ *
+ * @export
+ * @class HelloWorldService
+ * @description The business logic
+ */
+export declare class HelloWorldService {
+    getHelloWorld(): string;
+    getHelloWorldAsync(): Promise<string>;
+}
